abde93e823a17b168143be44b7a3927454807bfd
917gunwoo <917gunwoo@gmail.com>
2024-08-26 14:47:03 +0900

    Add files via upload

--- pyCoding01/learning01.py
+++ pyCoding01/learning01.py
@@ -1,4 +1,12 @@
-print('Hello world!!') 
 
-LeeDongJun="1223456"
 
+#print(10+20) 
+#print('10'+'20')
+
+#num1=int(input('첫번쨰 숫자 입력:'))
+#num2=int(input('두번쨰 숫자 입력:'))
+#print('두 수의 합:',num1+num2)
+
+
+age=input()
+print()
