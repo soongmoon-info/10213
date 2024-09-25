86eec86cdbb0a61cc794c56bdf40e78661df0aa6
917gunwoo <917gunwoo@gmail.com>
2024-09-25 11:45:51 +0900

    Add files via upload

---
+++ pyCoding03/learning02.py
@@ -0,0 +1,12 @@
+#import numpy as np
+
+#tempArr = np.array([10,20,30])
+tempList =[10,20,30]
+
+#print(tempArr*3)
+print(tempList*3)
+
+
+a = '안녕하세요'
+print('하' in a)
+print('하' in [10,20,30,40,50])
