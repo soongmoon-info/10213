d9d7c9969f6107eb6f23da502cef9e856e21f4c5
917gunwoo <917gunwoo@gmail.com>
2024-10-02 11:49:42 +0900

    Add files via upload

---
+++ pyCoding05/learning01.py
@@ -0,0 +1,28 @@
+#함수
+# def printHi():
+#   print('hi!')
+
+#다양한 명령들 
+
+# printHi()
+# print('hi!')
+# input('hi!')
+# len('hi!')
+
+
+
+def numSum(a,b,c):
+  b=20
+  return a,b,c
+  
+def numInput():
+  b=int(input('숫자 입력'))
+  return b
+
+x,y,z= numSum(10,20,30)
+print('안녕','하세요')
+print('안녕'+'하세요')
+
+num = 10
+num = num+10
+num += 10
